b08f5cfa2301c5281e01c867057af8671fb19409
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-13 14:49:45 +0100

    dev-Add tasks table columns and constructor attributes - Entity/Task.java

--- src/main/java/com/example/brief_resourcium_optima_beta/Entity/Task.java
+++ src/main/java/com/example/brief_resourcium_optima_beta/Entity/Task.java
@@ -2,7 +2,7 @@ package com.example.brief_resourcium_optima_beta.Entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="employees")
+@Table(name="tasks")
 public class Task {
 
     @Id
@@ -11,29 +11,30 @@ public class Task {
     private int id;
     @Column(name = "name")
     private String name;
-    @Column(name = "last_name")
-    private String last_name;
-    @Column(name = "user_name")
-    private String user_name;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "password")
-    private String password;
-    @Column(name = "position")
-    private int position;
-    @Column(name = "date")
-    private String date;
 
-    public Task(String name, String last_name, String user_name, String email, String password, int position, String date) {
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "limit_date")
+    private String limit_date;
+
+    @Column(name = "priority")
+    private String priority;
+
+    @Column(name = "employee")
+    private String employee;
+
+    @Column(name = "status")
+    private String status;
+
+    public Task(int id, String name, String description, String limit_date, String priority, String employee, String status) {
         this.name = name;
-        this.last_name = last_name;
-        this.user_name = user_name;
-        this.email = email;
-        this.password = password;
-        this.position = position;
-        this.date = date;
+        this.description = description;
+        this.limit_date = limit_date;
+        this.priority = priority;
+        this.employee = employee;
+        this.status = status;
     }
-
     public Task() {
 
     }
