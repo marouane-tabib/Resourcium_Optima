595478d425e89324324e3376b739ed17fb33992a
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-13 16:28:54 +0100

    dev-Make src/main/java/com/example/brief_resourcium_optima_beta/Departements.java file

---
+++ src/main/java/com/example/brief_resourcium_optima_beta/Departements.java
@@ -0,0 +1,40 @@
+package com.example.brief_resourcium_optima_beta;
+
+
+import com.example.brief_resourcium_optima_beta.Entity.Departement;
+import com.example.brief_resourcium_optima_beta.Entity.Task;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "tasks", value = "/tasks")
+public class Departements extends HttpServlet {
+    private String message;
+    private EntityManagerFactory entityManagerFactory = null;
+    private EntityManager entityManager = null;
+
+    public void init() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Departement departement = new Departement("Create UML", "Empty...", "marouane");
+        entityManager.getTransaction().begin();
+        entityManager.persist(departement);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void destroy() {
+    }
+
+}
